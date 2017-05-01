215ac7aae3ec5972349694660e2bbe6cf53bb6fd
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2017-05-01 18:17:09 +0530

    modified grid interval

--- MachineLearning_Udemy_SDS/Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression.py
+++ MachineLearning_Udemy_SDS/Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression.py
@@ -31,7 +31,7 @@ regressor.fit(X, y)
 y_pred = regressor.predict(6.5)
 
 # Visualising the Decision Tree Regression results (higher resolution)
-X_grid = np.arange(min(X), max(X), 0.001)
+X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
